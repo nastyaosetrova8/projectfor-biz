f2dcd71dcdbc54f043753369f1dad35332f53082
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-04-22 11:21:00 +0200

    add favicon, toast styled

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = 'https://db-projectfor.onrender.com/api';
-const baseUrl = 'http://localhost:3000/api';
+const baseUrl = "https://db-projectfor.onrender.com/api";
 
 export const instance = axios.create({
-    baseURL: baseUrl,
-  });
-
-
-  
-  
+  baseURL: baseUrl,
+});
--- src/shared/components/NotificationToastify/NotificationToast.styled.js
+++ src/shared/components/NotificationToastify/NotificationToast.styled.js
@@ -5,31 +5,33 @@ export const ToastContainerStyled = styled(ToastContainer)`
   width: 400px;
 
   .Toastify__toast-container {
-    background: green;
+    background: #ffffff;
   }
 
   .Toastify__toast--success {
-    background: green;
+    background: #ffffff;
+    color: #41ddd3;
   }
 
   .Toastify__toast--error {
-    background: green;
+    background: #ffffff;
+    color: #ff0000;
   }
 
   .Toastify__toast--info {
-    background: green;
+    background: #ffffff;
+    color: #0000ff;
   }
 
   .Toastify__toast-body {
-    width: 400px;
+    width: 300px;
     height: 80px;
-    font-size: 22px;
+    font-size: 18px;
     text-align: center;
-    color: black;
   }
 
   @media screen and (max-width: 768px) {
-    max-width: 280px;
+    max-width: 260px;
 
     .Toastify__toast-body {
       font-size: 16px;
